6d46974d39c85fa12a090f60c45de93c682876ba
kota <e235757@ie.u-ryukyu.ac.jp>
2023-12-11 17:00:17 +0900

    1st

---
+++ Report5.java
@@ -0,0 +1,23 @@
+public class Report5 {
+    public static void main(String[] args) {
+         String str = null;
+        System.out.println(str.length());        
+    }
+
+}
+
+/*public class Report5 {
+    public static void main(String[] args) {
+        // 条件1: String型変数 str を宣言し、初期値として null を代入する。
+        String str = null;
+
+        // 条件2: str.length() の内容を表示しようとする。
+        try {
+            System.out.println("Stringの長さ: " + str.length());
+        } catch (NullPointerException e) {
+            // NullPointerExceptionが発生した場合の処理
+            System.err.println("NullPointerExceptionが発生しました。");
+            e.printStackTrace();
+        }
+    }
+}*/
